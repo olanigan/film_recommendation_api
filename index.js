5496592374b4fb2842f971055b20ab8d7445c6ad
Ibrahim Olanigan <salalite@gmail.com>
2017-10-20 16:19:32 -0400

    Added Models for Film and Genre

--- index.js
+++ index.js
@@ -3,20 +3,44 @@ const sqlite = require('sqlite'),
       request = require('request'),
       express = require('express'),
       app = express();
+  
+const { PORT=3001, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
-const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
+const sequelize = new Sequelize(DB_PATH, '', '', {dialect: 'sqlite'})
 
-// START SERVER
-Promise.resolve()
+// MODELS
+const Film = sequelize.define('films', 
+{
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    title: Sequelize.STRING,
+    releaseDate: Sequelize.DATE,
+    tagline: Sequelize.STRING,
+    revenue: Sequelize.BIGINT,
+    budget: Sequelize.BIGINT,
+    runtime: Sequelize.INTEGER,
+    originalLanguage: Sequelize.STRING,
+    status: Sequelize.STRING,
+    genreId: Sequelize.INTEGER,
+});
+
+const Genre = sequelize.define('genres', 
+{
+    id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
+    name: Sequelize.STRING
+});
+
+
+  // START SERVER
+  Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
-// ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
+  // ROUTES
+  app.get('/films/:id/recommendations', getFilmRecommendations);
 
-// ROUTE HANDLER
-function getFilmRecommendations(req, res) {
-  res.status(500).send('Not Implemented');
-}
+  // ROUTE HANDLER
+  function getFilmRecommendations(req, res) {
+    res.status(500).send('Not Implemented');
+  }
 
 module.exports = app;
